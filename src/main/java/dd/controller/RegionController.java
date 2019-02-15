954516aeed849760711eabacd902d647b1aba7dc
Manny <egalli64@gmail.com>
2019-02-15 15:34:20 +0100

    Log added

--- src/main/java/dd/controller/RegionController.java
+++ src/main/java/dd/controller/RegionController.java
@@ -1,5 +1,7 @@
 package dd.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +11,15 @@ import dd.model.RegionRepository;
 
 @Controller
 public class RegionController {
+	private static final Logger logger = LoggerFactory.getLogger(RegionController.class);
+
 	@Autowired
 	RegionRepository repository;
 
 	@GetMapping("/regions")
 	public String getRegions(Model model) {
+		logger.info("getRegions");
+		
 		model.addAttribute("data", repository.findAll());
 		return "regions";
 	}
