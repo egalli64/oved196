6259aa9d01366cce95dee26ec206df1a79ba2df2
Luca <marascio1993@gmail.com>
2019-02-19 12:07:57 +0100

    codercontroller

--- src/main/java/dd/red/RedCoderController.java
+++ src/main/java/dd/red/RedCoderController.java
@@ -5,15 +5,13 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-
 import dd.red.model.RedCoder;
-
 
 import dd.red.model.RedCoderRepository;
 import dd.red.model.RedTeam;
@@ -25,35 +23,59 @@ public class RedCoderController {
 	@Autowired
 	RedCoderRepository repository;
 
+	private String findAll(Model model) {
+		logger.trace("findAll()");
+		model.addAttribute("data", repository.findAll());
+		return "/red/teams";
+	}
+
+	private void save(RedCoder team, Model model) {
+		logger.trace("save()");
+		try {
+			repository.save(team);
+		} catch (DataAccessException dae) {
+			String message = "Can't give name " + team.getName() + " to ";
+			if (team.getId() != 0) {
+				message += " team " + team.getId();
+			} else {
+				message += " il tuo cambiamneto";
+			}
+			logger.error(message);
+			model.addAttribute("msg", message);
+		}
+	}
+
 	@GetMapping("/red/coders")
 	public String getCoders(Model model) {
 		logger.trace("red getCoders");
-		
+
 		model.addAttribute("data", repository.findAll());
 		return "/red/coders";
-	}	
-	
-	@GetMapping("/red/setTeam")
-	public String setTeam (Model model, @RequestParam long id, @RequestParam RedTeam team) {
-		String data;
-		if ( repository.existsById(id)) {
-			Optional<RedCoder> coder = repository.findById(id);
-			RedCoder test = coder.get();
-			test.setTeam(team);
-		
-			
-			//model.addAttribute("data", repository.findAll());
-			data = test.toString();
-			return data;
-			
-		} else {
-			
-			data= "la persona legata a quell'id non esiste";
-			return data;
-		}
-		
-		
-		
-		
 	}
+
+	@GetMapping("/red/setTeam")
+	public String setTeam(Model model, @RequestParam long id, @RequestParam RedTeam test) {
+
+		logger.trace("setTeam()");
+		Optional<RedCoder> opt = repository.findById(id);
+		if (opt.isPresent()) {
+			RedCoder team = opt.get();
+			logger.debug(String.format("Change team %s as %s", team.getName(), test));
+			team.setTeam(test);
+			save(team, model);
+
+		} else {
+			String message = String.format("Can't save team %d: not found", id);
+			logger.error(message);
+			model.addAttribute("msg", message);
+		}
+
+		return findAll(model);
+
+	}
+	
+	
+
 }
+	
+
