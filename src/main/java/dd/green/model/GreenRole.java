fdb4ee741937f88781d0d59f99c8d644d16ad21e
carlo <carlo@mail.com>
2019-02-20 17:10:53 +0100

    154

--- src/main/java/dd/green/model/GreenRole.java
+++ src/main/java/dd/green/model/GreenRole.java
@@ -1,12 +1,8 @@
 package dd.green.model;
 
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.*;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -19,7 +15,31 @@ public class GreenRole {
 
 	@Column(name = "ROLE_NAME")
 	private String name;
+	
+	@Column(name = "CODER_ID")
+	private long id_cod;	
 
+
+	public GreenRole(long id, long id_cod) {
+		super();
+		this.id = id;
+		this.id_cod = id_cod;
+	}
+
+	public GreenRole(long id, String name, long id_cod) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.id_cod = id_cod;
+	}
+
+	public long getId_cod() {
+		return id_cod;
+	}
+
+	public void setId_cod(long id_cod) {
+		this.id_cod = id_cod;
+	}
 
 	public GreenRole(String name) {
 		super();
@@ -52,7 +72,8 @@ public class GreenRole {
 
 	@Override
 	public String toString() {
-		return "GreenRole [id=" + id + ", name=" + name + ", coders=" + "]";
+		return "GreenRole [id=" + id + ", name=" + name + ", id_cod=" + id_cod + "]";
 	}
 
-}
+	}
+
