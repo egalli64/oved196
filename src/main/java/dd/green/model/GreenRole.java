bb8be416cc90c97f0ab3b7e06e45f8dbcc8c654e
riccardo <garritanoriccg@gmail.com>
2019-02-21 17:47:51 +0100

    fix id sql problems

--- src/main/java/dd/green/model/GreenRole.java
+++ src/main/java/dd/green/model/GreenRole.java
@@ -1,7 +1,10 @@
 package dd.green.model;
 
+import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,6 +13,7 @@ import javax.persistence.Table;
 public class GreenRole {
 	@Id
 	@Column(name = "ROLE_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 
