eb3984b9c29717052ee153ecfc920cfe9022b43e
luana <gigliotti.luana@libero.it>
2019-02-21 12:38:40 +0100

    half order works

--- src/main/java/dd/blue/model/BlueCoderRepository.java
+++ src/main/java/dd/blue/model/BlueCoderRepository.java
@@ -1,9 +1,24 @@
 package dd.blue.model;
 
+import java.util.List;
+
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlueCoderRepository extends CrudRepository<BlueCoder, Integer> {
+	
+	public List<BlueCoder> findAllByOrderByIdCoder();
+
+	public List<BlueCoder> findAllByOrderByFirstname();
+
+	public List<BlueCoder> findAllByOrderByLastname();
+
+//	public List<BlueCoder> findAllByOrderById();
+//
+//	public List<BlueCoder> findAllOrderByRole();
+
+	//public List<BlueCoder> findAll(Sort sort);
+
 }
