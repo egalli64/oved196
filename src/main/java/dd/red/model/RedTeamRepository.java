8405df7bbdb6a119a8343091934d59b50ec0a0ca
Luca <marascio1993@gmail.com>
2019-02-22 16:06:38 +0100

    css perfetto

--- src/main/java/dd/red/model/RedTeamRepository.java
+++ src/main/java/dd/red/model/RedTeamRepository.java
@@ -2,6 +2,7 @@ package dd.red.model;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,7 @@ public interface RedTeamRepository extends CrudRepository<RedTeam, Long> {
 	
 	public Optional<RedTeam> findByName(String name);
 	public Optional<RedTeam> deleteByName (String name);
+	
+	/*@Query ("select t.name from red_team t where t.id>1")
+	public Iterable<RedTeam> findAllById();*/
 }
