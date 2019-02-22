db69cf5c39703b0b28a7fc2371fbc628294adf95
riccardo <garritanoriccg@gmail.com>
2019-02-22 15:15:15 +0100

    greencodercontroller fix 1

--- src/main/java/dd/green/GreenCoderController.java
+++ src/main/java/dd/green/GreenCoderController.java
@@ -59,12 +59,12 @@ public class GreenCoderController {
 		try {
 			repositoryCoders.save(coder);
 		} catch (DataAccessException dae) {
-			String message = "Non posso aggiungere " + coder.getName() + " al ";
-			if (coder.getId() != 0) {
-				message += " coder " + coder.getId();
-			} else {
-				message += " il tuo nuovo coder";
-			}
+			String message = "Non puoi! Il ruolo " + coder.getRoles() + "è già esistente. ";
+//			if (coder.getId() != 0) {
+//				message += " coder " + coder.getId();
+//			} else {
+//				message += " il tuo nuovo coder";
+//			}
 			logger.error(message);
 			model.addAttribute("msg", message);
 		}
@@ -82,12 +82,15 @@ public class GreenCoderController {
 
 		if (optCoders.isPresent()) {
 			GreenCoder coder = optCoders.get();
-			logger.debug(String.format("Changed team %s as %s", coder.getName(), id_team));
+			logger.debug(String.format("Squadra %s rinominato in %s", coder.getTeam(), id_team));
 			GreenTeam team = new GreenTeam(id_team);
 			coder.setTeam(team);
 			save(coder, model);
+			String message = coder.getName() + " " + coder.getSurname() + " è passato nella squadra " + team.getName();
+			model.addAttribute("msg", message);
 		} else {
-			String message = String.format("Can't change team %d: not found", id_team);
+			GreenCoder coder = optCoders.get();
+			String message = coder.getName() + " " + coder.getSurname() + "è già nella squadra " + coder.getTeam().getName();
 			logger.error(message);
 			model.addAttribute("msg", message);
 		}
@@ -97,9 +100,8 @@ public class GreenCoderController {
 
 	@GetMapping("/green/coders/create_role")
 	public String create( //
-			@RequestParam long id_coder, //
-			@RequestParam long id_role, //
-
+			@RequestParam long id_coder, 
+			@RequestParam long id_role, 
 			Model model) {
 		logger.trace("create()");
 
@@ -117,6 +119,8 @@ public class GreenCoderController {
 		if (check == false) {
 			rol_cod.add(role);
 			repositoryCoders.save(coder);
+			String message = coder.getRoles() + " creato";
+			model.addAttribute("msg", message);
 		}
 
 		return findAll(model);
@@ -134,6 +138,8 @@ public class GreenCoderController {
 
 		rol_cod.removeIf(r -> r.getId() == id_role);
 		repositoryCoders.save(coder);
+		String message = "Il ruolo " + coder.getRoles() + "è stato rimosso";
+		model.addAttribute("msg", message);
 
 		return findAll(model);
 	}
@@ -162,7 +168,8 @@ public class GreenCoderController {
 
 		rol_cod.add(role);
 		repositoryCoders.save(coder2);
-
+		String message = coder2.getName() + " " +  coder2.getSurname() + " inserito con successo";
+		model.addAttribute("msg", message);
 		return findAll(model);
 
 	}
@@ -175,8 +182,10 @@ public class GreenCoderController {
 		
 		try {
 			repositoryCoders.deleteById(id_coder);
+			String message = "coder rimosso";
+			model.addAttribute("msg", message);
 		} catch (DataAccessException dae) {
-			String message = String.format("Non puoi eliminare");
+			String message = String.format("Non puoi eliminare il coder selezionato");
 			logger.error(message);
 			model.addAttribute("msg", message);
 		}
